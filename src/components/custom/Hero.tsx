7d97ee3e89f193152c235a50a92e3131f4a3afe2
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-10 12:30:18 +0600

    added dynamic hero section input types

--- src/components/custom/Hero.tsx
+++ src/components/custom/Hero.tsx
@@ -15,7 +15,9 @@ import {
 import { GoTriangleDown } from 'react-icons/go';
 
 export default function Hero() {
-  const [position, setPosition] = React.useState('bottom');
+  const [position, setPosition] = React.useState('');
+
+  console.log(position);
 
   return (
     <section className="relative max-w-7xl mx-auto w-full overflow-hidden">
@@ -34,23 +36,70 @@ export default function Hero() {
                 variant="outline"
                 className="flex text-md py-6 text-gray-500 justify-between border-blue-300 border"
               >
-                Pick your type of project here
+                {position ? `${position}` : 'Pick your type of project here'}
                 <GoTriangleDown className="fill-blue-500 " />
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-70 md:min-w-3xl">
-              <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+              <DropdownMenuLabel>
+                Pick your type of project here
+              </DropdownMenuLabel>
               <DropdownMenuSeparator />
               <DropdownMenuRadioGroup
                 value={position}
                 onValueChange={setPosition}
               >
-                <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
-                <DropdownMenuRadioItem value="bottom">
-                  Bottom
+                <DropdownMenuRadioItem value="pick-your-type-of-project-here">
+                  Pick your type of project here
                 </DropdownMenuRadioItem>
-                <DropdownMenuRadioItem value="right">
-                  Right
+                <DropdownMenuRadioItem value="fintech-application">
+                  Fintech Application
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="data-mining-tool">
+                  Data Mining Tool
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="open-banking-app">
+                  Open Banking App
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="marketplace">
+                  Marketplace
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="api">API</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="analytics-bi-dashboard">
+                  Analytics / BI Dashboard
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="community-social-network">
+                  Community / Social Network
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="artificial-intelligence-application">
+                  Artificial Intelligence Application
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="ai-on-the-edge">
+                  AI on the edge
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="music-entertainment-app">
+                  Music & Entertainment App
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="regtech-software">
+                  Regtech Software
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="system-integration-project">
+                  System Integration Project
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="blockchain-app">
+                  Blockchain App
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="lifestyle-app">
+                  Lifestyle App
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="health-fitness-app">
+                  Health & Fitness App
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="travel-app">
+                  Travel App
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="sports-app">
+                  Sports App
                 </DropdownMenuRadioItem>
               </DropdownMenuRadioGroup>
             </DropdownMenuContent>
